3a74e30614d0a37c72f077c47121e0678df72cb9
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-26 16:27:36 -0500

    px2 on the outer div

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@
 
 function HomePage() {
   return (
-    <div className="h-screen flex flex-col items-center justify-center text-white border border-blue-400">
-      <h1 className="text-4xl font-bold mb-20 border border-yellow-400 px-2">ChatGPT4</h1>
+    <div className="h-screen flex flex-col items-center justify-center text-white border border-blue-400 px-2">
+      <h1 className="text-4xl font-bold mb-20 border border-yellow-400 ">ChatGPT4</h1>
     </div>
   )
 }
