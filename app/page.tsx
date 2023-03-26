8df5ddb367a5024aa2d9dd953ea9f58780419d4d
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-26 17:01:49 -0500

    add some borders

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,8 @@ function HomePage() {
     <div className="h-screen flex flex-col items-center justify-center text-white border border-blue-400 px-2">
       <h1 className="text-4xl font-bold mb-20 border border-yellow-400 ">ChatGPT4</h1>
 
-      <div>
-        <div>
+      <div className="border border-blue-500">
+        <div className="flex flex-col items-center justify-center border border-red-500">
           {/* Sun Icon */}
           <h2>Examples</h2>
         </div>
