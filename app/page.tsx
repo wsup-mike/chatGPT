464ebdb322de2eb9aa606e4f0471571fa417950d
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-26 16:39:58 -0500

    Examples div

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,13 @@ function HomePage() {
   return (
     <div className="h-screen flex flex-col items-center justify-center text-white border border-blue-400 px-2">
       <h1 className="text-4xl font-bold mb-20 border border-yellow-400 ">ChatGPT4</h1>
+
+      <div>
+        <div>
+          {/* Sun Icon */}
+          <h2>Examples</h2>
+        </div>
+      </div>
     </div>
   )
 }
