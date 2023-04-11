6031ae226acac2cf281d9b6c789e064a8e04c51c
Mike Ramos <michaelangeloram8@gmail.com>
2023-04-11 11:45:18 -0500

    Remove messenger

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 function HomePage() {
   return (
     <div className="h-screen flex flex-col items-center justify-center text-white border border-blue-400 px-2">
-      <h1 className="text-4xl font-bold mb-20 border border-yellow-400 ">ChatGPT4</h1>
+      <h1 className="text-4xl font-bold mb-20 border border-yellow-400 ">ChatGPT</h1>
 
       <div className="border border-blue-500">
         <div className="flex flex-col items-center justify-center mb-5 border border-red-500">
