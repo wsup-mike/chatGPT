c7177eee3d436cf0cb491219b5a6b08bf64de3b4
Mike Ramos <michaelangeloram8@gmail.com>
2023-04-11 12:21:34 -0500

    Remove all borders for now

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,10 @@ import { SunIcon } from '@heroicons/react/24/outline'
 function HomePage() {
   return (
     <div className="h-screen flex flex-col items-center justify-center text-white border border-blue-400 px-2">
-      <h1 className="text-2xl font-bold mb-14 border border-yellow-400 ">ChatGPT</h1>
+      <h1 className="text-2xl font-bold mb-14">ChatGPT</h1>
 
-      <div className="border border-blue-500">
-        <div className="flex flex-col items-center justify-center mb-5 border border-red-500">
+      <div>
+        <div className="flex flex-col items-center justify-center mb-5">
           {/* Sun Icon */}
           <SunIcon className='h-6 w-6'/>
           <p className='text-xs'>Examples</p>
@@ -17,8 +17,21 @@ function HomePage() {
           <p className="infoText">"What is the difference between a dog and a cat?"</p>
           <p className="infoText">"What is the color of the sun?"</p>
         </div>
-
       </div>
+      <div>
+        <div className="flex flex-col items-center justify-center mb-5">
+          {/* Sun Icon */}
+          <SunIcon className='h-6 w-6'/>
+          <p className='text-xs'>Examples</p>
+        </div>
+
+        <div className="space-y-1">
+          <p className="infoText">"Explain something to me."</p>
+          <p className="infoText">"What is the difference between a dog and a cat?"</p>
+          <p className="infoText">"What is the color of the sun?"</p>
+        </div>
+      </div>
+      
     </div>
   )
 }
