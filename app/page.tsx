00ff1a67b66d8048c7f7e33566bb4f3d6877c769
Mike Ramos <michaelangeloram8@gmail.com>
2023-04-11 12:23:07 -0500

    BoltIcon

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { SunIcon } from '@heroicons/react/24/outline'
+import { BoltIcon, SunIcon } from '@heroicons/react/24/outline'
 
 function HomePage() {
   return (
@@ -9,6 +9,19 @@ function HomePage() {
         <div className="flex flex-col items-center justify-center mb-5">
           {/* Sun Icon */}
           <SunIcon className='h-6 w-6'/>
+          <p className='text-xs'>Examples</p>
+        </div>
+
+        <div className="space-y-1">
+          <p className="infoText">"Explain something to me."</p>
+          <p className="infoText">"What is the difference between a dog and a cat?"</p>
+          <p className="infoText">"What is the color of the sun?"</p>
+        </div>
+      </div>
+      <div>
+        <div className="flex flex-col items-center justify-center mb-5">
+          {/* Sun Icon */}
+          <BoltIcon className='h-6 w-6'/>
           <p className='text-xs'>Examples</p>
         </div>
 
