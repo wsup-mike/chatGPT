eaaebc2cfc05074a1edbdac0acb7ac572d9abab8
Mike Ramos <michaelangeloram8@gmail.com>
2023-04-12 11:01:57 -0500

    NewChatBut-fix duration

--- components/NewChatButton.tsx
+++ components/NewChatButton.tsx
@@ -2,7 +2,7 @@ import { PlusIcon } from "@heroicons/react/24/solid";
 
 function NewChatButton() {
   return (
-    <div className="flex items-center border border-gray-600 chatRow">
+    <div className="flex border border-gray-600 chatRow">
         <PlusIcon className="h-4 w-4" />
         <p>New chat</p>
     </div>
