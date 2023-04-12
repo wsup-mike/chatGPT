65028df59a47798755f5f2d79594310071b3d990
Mike Ramos <michaelangeloram8@gmail.com>
2023-04-12 10:37:02 -0500

    NewChatBut-stlye it up

--- components/NewChatButton.tsx
+++ components/NewChatButton.tsx
@@ -2,7 +2,7 @@ import { PlusIcon } from "@heroicons/react/24/solid";
 
 function NewChatButton() {
   return (
-    <div>
+    <div className="flex items-center border border-gray-600 chatRow">
         <PlusIcon className="h-4 w-4" />
         <p>New chat</p>
     </div>
