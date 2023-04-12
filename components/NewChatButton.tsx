9b487a18511e884961c9cc1ea2beaf633b1ef9a4
Mike Ramos <michaelangeloram8@gmail.com>
2023-04-12 10:32:12 -0500

    import n use PlusIcon

--- components/NewChatButton.tsx
+++ components/NewChatButton.tsx
@@ -1,6 +1,9 @@
+import { PlusIcon } from "@heroicons/react/24/solid";
+
 function NewChatButton() {
   return (
     <div>
+        <PlusIcon className="h-4 w-4" />
         <p>New chat</p>
     </div>
   )
