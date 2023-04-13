4ff8a1d2608f1f8c01cba259868c99be0814f7b0
Mike Ramos <michaelangeloram8@gmail.com>
2023-04-13 12:10:17 -0500

    Cr8 new component SessionProvider.tsx

---
+++ components/SessionProvider.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import { Session } from 'next-auth'
+import { SessionProvider as Provider } from 'next-auth/react'
+
+export function SessionProvider() {
+  return (
+    <Provider>
+        {children}
+    </Provider>
+  )
+}
+
