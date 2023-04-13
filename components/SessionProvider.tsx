84cec9d5848a4f1f21cdfad68660f4e4b979ed66
Mike Ramos <michaelangeloram8@gmail.com>
2023-04-13 12:32:09 -0500

    SessionProvider-Add type definitions

--- components/SessionProvider.tsx
+++ components/SessionProvider.tsx
@@ -3,6 +3,11 @@
 import { Session } from 'next-auth'
 import { SessionProvider as Provider } from 'next-auth/react'
 
+type Props = {
+    children: React.ReactNode;
+    session: Session | null;
+}
+
 export function SessionProvider() {
   return (
     <Provider>
